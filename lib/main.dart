f8e9d04f423d53b16c31feafffd20f43d283be9a
Andrei Dodu <andrei.dodu@hotmail.it>
2023-09-02 11:11:20 +0200

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:been/screen/country_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.light,
+    seedColor: const Color.fromARGB(255, 60, 255, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme().copyWith().apply(),
+);
+
+final themeDark = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 60, 255, 0),
+  ),
+  textTheme:
+      GoogleFonts.latoTextTheme().copyWith().apply(bodyColor: Colors.white),
+);
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: theme,
+      themeMode: ThemeMode.system,
+      darkTheme: themeDark,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: const Scaffold(
         body: CountryScreen(),
       ),
     ),
